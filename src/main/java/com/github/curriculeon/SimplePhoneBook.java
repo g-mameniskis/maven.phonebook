70036681259f761edeb49d20c3980cefca31eff8
unknown <g.mameniskis1@gmail.com>
2019-12-19 15:33:10 -0500

    first commit

--- src/main/java/com/github/curriculeon/SimplePhoneBook.java
+++ src/main/java/com/github/curriculeon/SimplePhoneBook.java
@@ -1,32 +1,46 @@
 package com.github.curriculeon;
 
+import java.util.*;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
 
 /**
  * Created by leon on 12/16/2019.
  */
 public class SimplePhoneBook {
+
+    private Map <String, String> phoneMap = new HashMap<String, String>();
+     private Object List;
+
     public void add(String name, String phoneNumberToAdd) {
+        phoneMap.put(name, phoneNumberToAdd);
 
     }
 
     public String get(String name) {
-        return null;
+
+        return name;
     }
 
     public Boolean hasEntry(String phoneNumber) {
-        return null;
+
+        return phoneMap.containsKey(phoneNumber);
     }
 
     public void remove(String name) {
 
+        phoneMap.remove(name);
     }
 
     public List<String> getAllContactNames() {
-        return null;
+        // https://www.mkyong.com/java8/java-8-convert-map-to-list/
+        return List<String> keysInList = new ArrayList<>(phoneMap.keySet());
+        keysInList.forEach(System.out::println);
     }
 
     public String reverseLookup(String phoneNumber) {
+
         return null;
     }
 }
